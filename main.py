80407c533de7fb0e8146d2d8c3798f961dfc50a7
Ng Cheryl <ng.cheryl@dhs.sg>
2013-03-25 00:01:00 +0800

    Updated code with edit and delete functions included

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Filename: main.py
 # Name: Ng Cheryl
-# Description: receive ticket orders from user and write to sqlite database
+# Description: receive ticket orders from user and write to datastore
 import webapp2
 import jinja2
 import os
@@ -8,6 +8,7 @@ import datetime
 from google.appengine.api import users	# Google account authentication
 from google.appengine.ext import db		# datastore
 from google.appengine.api import mail	# send email
+import cgi
 
 # initialize template
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -30,36 +31,33 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
-            query = Person.all().filter("email=", user.email())
-            #query = Person.gql("WHERE email = :1", user.email())
+            query = Person.gql("WHERE email = :1", users.get_current_user().email())
             result = query.fetch(1)
+            template = jinja_environment.get_template('index.html')
             if result: # record exists
-                person = result[0]
-		url = users.create_logout_url(self.request.uri)
+                student = result[0]
+                person = student.name + "'s"
+                url = users.create_logout_url(self.request.uri)
                 url_linktext = "Logout"
             else: # not found
                 person = False
                 url = users.create_logout_url(self.request.uri)
-                url_linktext = "Logout"
+                url_linktext = "Unauthorised"
+                template = jinja_environment.get_template('404.html')
         else: # not logged in
             person = ""
             url = users.create_login_url(self.request.uri)
             url_linktext = "Login"
-
+            template = jinja_environment.get_template('404.html')
         template_values = {
 	    'person': person,
 	    'url': url,
 	    'url_linktext': url_linktext,
 		}
-
-        if user:
-            template = jinja_environment.get_template('index.html')
-        else:
-            template = jinja_environment.get_template('404.html')
         self.response.write(template.render(template_values))
 
-class Order(db.Model):
-    """Models an individual ticket order with name, email, number of tickets and date."""
+class Order(db.Expando):
+    """Ticket order with name, class, email, number of tickets and date"""
     name = db.StringProperty(required=True)
     user_class = db.StringProperty(required=True)
     email = db.EmailProperty(required=True)
@@ -76,35 +74,60 @@ class OrderTickets(webapp2.RequestHandler):
     def post(self):
         if self.request.get("confirm"):
             user = users.get_current_user()
-            query = Person.all().filter("email=", user.email())
-            #query = Person.gql("WHERE email = :1", user.email())
-            result = query.fetch(1)
-            if result:
-                person = result[0]
-                t = Order(name = person.name, user_class = person.user_class, email = person.email, ticketNum = cgi.escape(self.request.get('tickets')))
-                t.put()
-            # go back to home page
-            self.redirect('/view')
+            if user:
+                query = Person.gql("WHERE email = :1", users.get_current_user().email())
+                result = query.fetch(1)
+                if result:
+                    person = result[0]
+                    tickets = int(self.request.get('tickets'))
+                    if tickets:
+                        # Create an order in the datastore
+                        t = Order(name = person.name, user_class = person.user_class, email = person.email, ticketNum = tickets)
+                        t.put()
+                        self.redirect('/')    
+                else:
+                    self.redirect('/order')            
+            
 
 class ViewOrder(webapp2.RequestHandler):
     def get(self):
-        orders = db.GqlQuery("SELECT * "
-                            "FROM Order "
-                            'WHERE email = :1',
-                            users.get_current_user().email())
-        if orders:
-	    for order in orders:
-                self.response.out.write('Number of tickets:')
-                self.response.out.write('<b>%s</b>' % cgi.escape(order.ticketNum))
+        query = Order.gql("WHERE email = :1", users.get_current_user().email()) 
+        result = query.fetch(1)
+        if result:
+            orders = result[0]
+            self.response.out.write('Number of tickets:')
         else:
-	    self.response.out.write('You have no orders')   
-        template_values = {}
+            orders = False
+            self.response.out.write('You have no orders')   
+        template_values = {'orders':orders}
         template = jinja_environment.get_template('view.html')
         self.response.write(template.render(template_values))
 
+class EditOrder(webapp2.RequestHandler):
+    def post(self):
+        if self.request.get("edit"):
+            updated_tickets = int(self.request.get('updated_tickets'))
+            query = Order.gql("WHERE email = :1", users.get_current_user().email())
+            result = query.fetch(1)
+            if result:
+                orders = result[0]
+                orders.ticketNum = updated_tickets
+                orders.put()
+                self.redirect('/')
+            else:
+                self.response.out.write('Error in saving.')
+                
+class DeleteOrder(webapp2.RequestHandler):
+    def post(self):
+        if self.request.get("delete"):
+            query = Order.gql("WHERE email = :1", users.get_current_user().email())
+            result = query.fetch(1)
+            db.delete(result)
+            self.redirect('/')
+        else:
+            self.response.out.write('Error in editing')
 # main
 # example of person
 # p0 = Person(pid='lim.ahseng',name='LIM AH SENG',user_class='13Y5C',email='lim.ahseng@person.com')
-# p0.put()
-                
-app = webapp2.WSGIApplication([('/', MainHandler), ('/order', OrderTickets), ('/view', ViewOrder)], debug=True)
+# p0.put()               
+app = webapp2.WSGIApplication([('/', MainHandler), ('/order', OrderTickets), ('/view', ViewOrder), ('/edit', EditOrder), ('/delete', DeleteOrder)], debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -24,9 +24,29 @@ function isNumeric(){
 	}
 }
 
-	
-$(document).ready(function(){	
-	// content to fade in
-	$('iframe').fadeIn('slow');
-});	
+// If the length of the element's string is 0 then display helper message
+function editNotEmpty(){
+	var ticketNum = document.getElementById('updated_tickets')
+	if(ticketNum.value.length == 0){
+		alert('Please enter a value');
+		ticketNum.focus(); // set the focus to this input
+		return false;
+	}
+	return true;
+}
+
+// if the element's string matches the regular expression it is all numbers
+function editIsNumeric(){
+	var ticketNum = document.getElementById('updated_tickets')
+	var numericExpression = /^[0-9]+$/;
+	if(ticketNum.value.match(numericExpression)){
+		alert('Thank you! Your changes have been saved')
+		return true;
+	}
+	else{
+		alert('Please enter a number');
+		ticketNum.focus();
+		return false;
+	}
+}
 
